fddfa35b8215ad5f23557d54a7928421c9db9f88
Moosest Bodez <martenpoder@pm.me>
2026-09-08 09:23:49 +0300

    add array examples

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 == 10;
-$test = 10 <= 10;
-$test = 10 >= 10;
-$test = 10 == "10";
-$test = 10 === "10";
-$test = 10 != 10;
-$test = 10 !== 10;
-$test = 0 == false;
-var_dump($test)
+$test = array(1, 2, 3);
+$test = [1, 2, 3];
+$test = [1, 'asdas', true, [1, 2, 3]];
+var_dump ($test[3][1]);
+$test = [
+    1,
+    'name' => 'Marten',
+    'age' => 33,
+    3,
+    'aadsas',
+    true,
+    100 => 'lol',
+    'troll',
+];
+var_dump($test['name']);
+array_push($test, 'added');
+$test[]= 'new value';
+$test['name'] = 'Marten';
+$test[1] = 99999;
+unset($test[3]);
+var_dump($test);
 ?>
