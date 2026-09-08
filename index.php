785dc405e349aa0f0dd33d5b2a602e2537323c80
Moosest Bodez <martenpoder@pm.me>
2026-09-08 09:59:02 +0300

    add loops example

--- index.php
+++ index.php
@@ -1,50 +1,33 @@
 <?php
-$num = 10;
-if($num > 10) {
-    var_dump('Bigger');
-}
-if($num > 10) {
-    var_dump('Bigger');
-} else {
-    var_dump('Smaller');
-}
-if($num > 10) {
-    var_dump('Bigger');
-} elseif($num < 10) {
-    var_dump('Smaller');
-} else {
-    var_dump('Equal');
-}
-$day = (int) date('w');
-$day = intval(date('w'));
-var_dump($day);
-if ($day === 0) {
-    var_dump('Sunday');
-} elseif($day === 1) {
-    var_dump('Monday');
-} elseif($day === 2) {
-    var_dump('Tuesday');
-} elseif($day === 3) {
-    var_dump('Wednesday');
-} elseif($day === 4) {
-    var_dump('Thursday');
-} elseif($day === 5 ||$day === 6) {
-    var_dump('Partyday');
-} else {
-    var_dump('Weirdday');
+for ($i = 0; $i<10; $i++){
+    var_dump($i);
 }
 
-switch ($day){
-    case 1:
-        var_dump('Monday');
-    case 2:
-        var_dump('Tuesday');
-    case 3:
-        var_dump('Wednesday');
-    case 4:
-        var_dump('Thursday');
-    case 4:
-        var_dump('Friday');
-
+for ($i = 9; $i>0; $i--){
+    var_dump($i);
 }
+
+for($i = 2; $i<1_000_000; $i*=2){
+    var_dump($i);
+}
+
+$time = time();
+$i=0;
+while(time() < $time+1){
+    var_dump($i++);
+}
+
+while(false) {
+    var_dump('WHILE');
+}
+
+do {
+    var_dump('DO');
+} while(false);
+
+$fruits = ['Apple', 'Banana', 'Cherry'];
+foreach($fruits as $key=>$fruit){
+    var_dump($key,$fruit);
+}
+
 ?>
