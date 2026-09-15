2c292f3755ef0dad5cffb876a7749165adaaa12f
Moosest Bodez <martenpoder@pm.me>
2026-09-15 10:34:13 +0300

    add visibilty example

--- index.php
+++ index.php
@@ -1,44 +1,91 @@
-
 <?php
 
-class Box {
+class Box
+{
     use HasSmell;
-    public $isOpen = false;
-    public $hasBeenOpened = false;
-    public function __construct(public $width, public $height, public $length) {
+
+    public bool $isOpen = false;
+    private bool $hasBeenOpened = false;
+
+    public function __construct(
+        private int $width,
+        private $height,
+        private $length
+    ) {
     }
-    public function open() {
+
+    public function setWidth(int $width): void
+    {
+        if ($width < 0) {
+            $this->width = 0;
+        }
+
+        $this->width = $width;
+    }
+
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    public function open(): void
+    {
         $this->isOpen = true;
     }
-    public function volume(){
+
+    public function volume()
+    {
         return $this->width * $this->height * $this->length;
     }
-}
 
-class MetalBox extends Box {
-    use HasColor, HasSmell;
-    public $weightPerUnit;
-
-    public function mass(){
-        return $this->weightPerUnit * $this->volume();
+    public function test1(): void
+    {
+        var_dump($this->hasBeenOpened);
     }
 }
 
-trait HasColor {
+class MetalBox extends Box
+{
+    use HasColor, HasSmell;
+
+    public $weightPerUnit;
+
+    public function mass()
+    {
+        return $this->weightPerUnit * $this->volume();
+    }
+
+    public function test2(): void
+    {
+        var_dump($this->hasBeenOpened);
+    }
+}
+
+trait HasColor
+{
     public $color;
-    public function showColor(){
+
+    public function showColor()
+    {
         return $this->color;
     }
 }
 
-trait HasSmell {
+trait HasSmell
+{
     public $smell;
-    public function sniff(){
+
+    public function sniff()
+    {
         return $this->smell;
     }
 }
 
-
-$metal1 = new MetalBox(1,2,3);
+$metal1 = new MetalBox(1, 2, 3);
 $metal1->weightPerUnit = 1;
 var_dump($metal1->mass(), $metal1);
+$metal1->isOpen = 'asdasd';
+var_dump($metal1->isOpen);
+$metal1->test1();
+$metal1->test2();
+var_dump($metal1->hasBeenOpened);
